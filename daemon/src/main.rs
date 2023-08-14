ebabc1aa242d3b4b0d46b35d0e5776705cc55bd3
Dmitry Ponyatov <dponyatov@gmail.com>
2023-08-14 12:40:35 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,1 +1,23 @@
-pub fn main() {}
+use std::io::{prelude::*, BufReader};
+use std::net::{TcpListener, TcpStream};
+
+pub fn main() {
+    println!("{}", config::url);
+    let listener = TcpListener::bind(config::bind).unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle(stream);
+    }
+}
+
+#[macro_use]
+extern crate lalrpop_util;
+
+lalrpop_mod!(pub http);
+
+fn handle(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let req = buf_reader.lines().next().unwrap().unwrap();
+    println!("{}", req);
+    http::GETParser::new().parse(&mut stream, &req).unwrap();
+}
